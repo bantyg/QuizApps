a833a44e26d7bc27357873c2437ef69f0c9c9db5
vikaspal <vikaspal@thoughtworks.com>
2015-01-10 15:26:16 +0530

    login (s-001) poojag_vikas login page work with dummy data and render to dashboard

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,30 @@
 var express = require('express');
-var quizModules = require('../module/quizModules.js');
+var quizModules = require('../modules/quizModules').init('./data/quiz.db');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.get('/login', function(req, res) {
+  res.render('login');
+});
+
+router.post('/login', function(req, res) {
+	var email = req.body.email;
+  quizModules.getEmailAndPassword(email,function(err, user) {
+    if (user && req.body.password == user.password) {
+      res.redirect('/dashboard/');
+    } else
+      res.render('login', {
+        error: 'invalid email or password'
+      });
+  });
+});
+
+router.get('/dashboard', function(req, res) {
+  res.render('dashboard');
 });
 
 module.exports = router;
