0c5c91b0a59337626b0a97060fcf99905d94cabe
vikaspal <vikaspal@thoughtworks.com>
2015-01-12 10:07:43 +0530

    S-001 vikasp_poojag login story completed

--- shellCommands/initdb.sh
+++ shellCommands/initdb.sh
@@ -1,6 +1,10 @@
+npm install
+rm -rf data
+rm -rf test/data
 mkdir data
 node scripts/initializeDB.js data/quiz.db 
 mkdir test/data
 node scripts/initializeDB.js test/data/quiz.db
 sqlite3 test/data/quiz.db < scripts/insertData.sql
+sqlite3 data/quiz.db < scripts/insertData.sql
 cp test/data/quiz.db test/data/quiz.db.backup
